5d4ac41500eb7e4f12d35fb090c7a7eef68d73a7
akinaybt <abekmambetova364@gmail.com>
2022-10-30 20:57:34 +0600

    Made changes in BookInstanceAdmin. They are:

--- catalog/admin.py
+++ catalog/admin.py
@@ -6,15 +6,29 @@ from .models import (
 
 class AuthorAdmin(admin.ModelAdmin):
     list_display = ('first_name', 'last_name', 'date_of_birth', 'date_of_death')
+    fields = ['last_name', 'first_name', 'date_of_birth', 'date_of_death']
+
+
+class BookInstanceInline(admin.TabularInline):
+    model = BookInstance
 
 
 class BookAdmin(admin.ModelAdmin):
     list_display = ('title', 'genre', 'language', Book.display_author)
     list_filter = ('genre', 'author')
+    inlines = [BookInstanceInline]
 
 
 class BookInstanceAdmin(admin.ModelAdmin):
-    list_filter = ('book', 'status')
+    list_display = ('book', 'status', 'borrower', 'due_back', 'id')
+    list_filter = ('book', 'status', 'due_back')
+
+    fieldsets = (
+        ('Экземпляр книги', {'fields': ('book', 'imprint', 'inv_nom')
+                             }),
+        ('Статус и окончание его действия', {'fields': ('status', 'due_back', 'borrower')
+                                             }),
+    )
 
 
 admin.site.register(Author, AuthorAdmin)
