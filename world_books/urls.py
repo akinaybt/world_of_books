7a476d92bc11b9e6d5e48b55aa2514cf0fa160ae
akinaybt <abekmambetova364@gmail.com>
2022-10-31 20:33:20 +0600

    I have added urls for models: BookInctance, Genre and Status

--- world_books/urls.py
+++ world_books/urls.py
@@ -5,12 +5,15 @@ from catalog import views
 
 router = DefaultRouter()
 router.register('authors', views.AuthorViewSet)
+router.register('books', views.BookViewSet)
+router.register('language', views.LanguageViewSet)
+router.register('genres', views.GenreViewSet)
+router.register('status', views.StatusViewSet)
+router.register('book_instance', views.BookInstanceViewSet)
 
 
 urlpatterns = [
     path('', include(router.urls)),
-    path('book-create/', views.BookListView.as_view()),
-    path('book-delete/<str:title>/', views.BookDestroyAPIView.as_view()),
     path('admin/', admin.site.urls),
     path('client/auth/', views.RegisterAPI.as_view(), name='registration'),
 ]
